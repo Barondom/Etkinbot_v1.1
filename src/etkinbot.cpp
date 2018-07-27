d8d2706bc5cd30fc78887e9fb626f208fbff6edf
cgtyylmz <etkinteknolojiler@Etkin-iMac.local>
2018-07-27 14:38:20 +0300

    edited js,s2e,lib

--- src/etkinbot.cpp
+++ src/etkinbot.cpp
@@ -3,7 +3,7 @@
 
 Yerleşik özellikler;
 
-# 2 adet encoderli Mikro dc motor ** motor 1
+# 2 adet encoderli Mikro dc motor
 # Kullanıcı butonu
 # Buzzer **
 # RGB Led ***
@@ -19,34 +19,115 @@ Yerleşik özellikler;
 		-Yön pini = 4 *
 		-Pwm pini = 11 *
  		-Encoder pini = 2
-- Buzzer
+* Buzzer
 		-pin 8 *
-- Çizgi izleme sensorleri
+* Çizgi izleme sensorleri
 		-Sağ sensör A6
 		-Sol Sensör A3
 
 --------------------------------------------------------------------------------
+Çağatay YILMAZ
 arge@etkinketnolojiler.com
 
+Doğukan TUNCER
+tasarım@etkinteknolojiler.com
 */
-
-//pid eklenecek
 
 //#include<avr/interrupt.h>
 #include "etkinbot.h"
 
 
-int counter =0, counter1 = 0;
-double timer0, timer1, timer2, timer3, timer4, timer5;
+volatile int counter =0, counter1 = 0;
+volatile double timer0, timer1, timer2, timer3, timer4, timer5;
 double timer6, timer7;
-double motor_speed = 0, motor_speed1 = 0;
+volatile double motor1_speed = 0, motor2_speed = 0;
 int motor_speed_theoric;
 void stop(bool urgentStop);
 void pwmWriteDistance_1(int out, int dir, int dist);
 void pwmWriteDistance_2(int out, int dir, int dist);
 void initINT();
-int distance1 = 0, distance2 = 0;
-double InputMotor1,OutputMotor1,SetpointMotor1, Kp, Ki, Kd, InputMotor2,OutputMotor2,SetpointMotor2;
+volatile int distance1 = 0, distance2 = 0;
+int distancein = 0;
+double InputMotor1,OutputMotor1,SetpointMotor1, InputMotor2,OutputMotor2,SetpointMotor2;
+double Kp, Ki, Kd;
+bool one_loop=0;
+
+//************************************************************
+//Interrupt 1
+//************************************************************
+ISR(INT0_vect)
+{
+	timer0 = micros();
+
+	if (counter == 0)
+		timer1 = timer0;
+
+	counter++;
+
+	if (counter == 7)
+	{
+		if (timer0 > timer1)
+		{
+			timer2 = timer0 - timer1;
+			motor2_speed = timer2 / 1000;
+			motor2_speed = motor2_speed / 1000;
+			motor2_speed = 60 / motor2_speed;
+			motor2_speed = motor2_speed / 50;
+
+			//************************************************************
+			//PID Setpoint Ayarlanıyor
+			//************************************************************
+			if (motor2_speed > motor1_speed)
+			{
+				SetpointMotor2 = map(motor1_speed, 0, 420, 0, 1023);
+			}
+			else
+			{
+				SetpointMotor2 = motor_speed_theoric;
+			}
+		}
+		counter = 0;
+		distance1++;
+	}
+}
+//************************************************************
+//Interrupt 2
+//************************************************************
+ISR(INT1_vect)
+{
+  timer3 = micros();
+
+  if (counter1 == 0)
+    timer4 = timer3;
+
+  counter1++;
+
+  if (counter1 == 7)
+  {
+    if (timer3 > timer4)
+    {
+      timer5 = timer3 - timer4;
+      motor1_speed = timer5 / 1000;
+      motor1_speed = motor1_speed / 1000;
+      motor1_speed = 60 / motor1_speed;
+      motor1_speed = motor1_speed / 50;
+
+		//************************************************************
+		//PID Setpoint Ayarlanıyor
+		//************************************************************
+      if (motor1_speed > motor2_speed)
+      {
+        SetpointMotor1 = map(motor2_speed, 0, 420, 0, 1023);
+      }
+      else
+      {
+        SetpointMotor1 = motor_speed_theoric;
+      }
+    }
+    counter1 = 0;
+    distance2++;
+  }
+}
 
 EtkinClass::EtkinClass(){
 	trig = 13;
@@ -62,13 +143,13 @@ EtkinClass::EtkinClass(){
 	cny70_L = A3;
 
 
-	pinMode(motor1_dir, OUTPUT); //cizildi
-	pinMode(motor2_dir, OUTPUT); //cizildi blue rgb
-	pinMode(motor1_pwm, OUTPUT); //cizildi
-	pinMode(motor2_pwm, OUTPUT); //cizildi green rgb
-	pinMode(rgb_red, OUTPUT); //cizildi red rgb
-	pinMode(rgb_green, OUTPUT); //cizildi
-	pinMode(rgb_blue, OUTPUT); //cizildi
+	pinMode(motor1_dir, OUTPUT);
+	pinMode(motor2_dir, OUTPUT);
+	pinMode(motor1_pwm, OUTPUT);
+	pinMode(motor2_pwm, OUTPUT);
+	pinMode(rgb_red, OUTPUT);
+	pinMode(rgb_green, OUTPUT);
+	pinMode(rgb_blue, OUTPUT);
 
 	Kp = 0.002;
 	Ki = 3;
@@ -83,82 +164,16 @@ EtkinClass::EtkinClass(){
 	myPID1->SetOutputLimits(0, 255);
 	myPID2->SetOutputLimits(0, 255);
 
-	sei();
+	initINT();
+
 	level = 0;
-	distance = 0;
+	distanceHcsr04 = 0;
 	duration = 0;
 	line_left = 0;
 	line_right = 0;
 
 }
 
-ISR(INT0_vect)
-{
-	timer0 = micros();
-	if (counter == 0)
-	{
-		timer1 = timer0;
-	}
-
-	counter++;
-
-	if (counter == 7)
-	{
-    	if (timer0 > timer1)
-    	{
-      		timer2 = timer0 - timer1;
-			motor_speed = timer2 / 1000;
-			motor_speed = motor_speed / 1000;
-			motor_speed = 60 / motor_speed;
-			motor_speed = motor_speed / 50;
-
-			if (motor_speed > motor_speed1)
-			{
-			  SetpointMotor1 = map(motor_speed1, 0, 420, 0, 1023);
-			}
-			else
-			{
-			  SetpointMotor1 = motor_speed_theoric;
-			}
-    	}
-		counter = 0;
-		distance1++;
-	}
-}
-
-ISR(INT1_vect)
-{
-	timer3 = micros();
-	if (counter1 == 0)
-	{
-		timer3 = timer4;
-	}
-
-	counter++;
-
-	if (counter1 == 7)
-	{
-    	if (timer3 > timer4)
-    	{
-      		timer5 = timer3 - timer4;
-			motor_speed1 = timer5 / 1000;
-			motor_speed1 = motor_speed1 / 1000;
-			motor_speed1 = 60 / motor_speed1;
-			motor_speed1 = motor_speed1 / 50;
-
-			if (motor_speed1 > motor_speed)
-			{
-			  SetpointMotor2= map(motor_speed, 0, 420, 0, 1023);
-			}
-			else
-			{
-			  SetpointMotor2 = motor_speed_theoric;
-			}
-    	}
-		counter1 = 0;
-		distance2++;
-	}
-}
 
 /* distancesensor()************************************************************
 *Hc-Sr04 Ultarasonik mesafe sensörü ile mesafe ölçümü.
@@ -171,19 +186,23 @@ double EtkinClass::distancesensor(){
 	delayMicroseconds(10);
 	digitalWrite(trig, LOW);
 	duration = pulseIn(echo, HIGH);
-	distance = duration / 58.2;
+	distanceHcsr04 = duration / 58.2;
 	delay(50);
-	return distance;
+	return distanceHcsr04;
 }
 
 /* linesensor()************************************************************
 *Robotun üzerinde bulunan iki adet Cny70 çizgi sensorleri ile, çizgi izleme
 yapılabilirinir.
 *Sınır değeri olarak 100 ayarlanmıştır.
-*
+*Metod Donus degerleri
+	-iki sensor siyah cizgide = 3
+	-sag sensor beyazda = 2
+	-sol sensor beyazda = 1
+	-ikisi de beyazda = 0
 ******************************************************************************/
 int EtkinClass::linesensor(){
-	int threshold_value  = 100;
+	int threshold_value  = 110;
 	line_left = analogRead(cny70_L);
 	line_right = analogRead(cny70_R);
 	if (line_left < threshold_value && line_right < threshold_value){
@@ -238,7 +257,6 @@ void EtkinClass::motor(int motor_id, int speed){
 /*move()***********************************************************/
 void EtkinClass::move(int direction, int speed)
 {
-
 /*
 	direction = 1 --> ileri(forward)
 	direction = 2 --> geri(backward)
@@ -269,36 +287,73 @@ void EtkinClass::move(int direction, int speed)
   	analogWrite(motor2_pwm, speed);
 }
 /*movePID()*************************************************************
-*move metodundan farkli olarak bu metod motorlarin hiz ve konum kontrolunu saglayarak dogrusal olarak hareket etmesi saglanmistir.
+*move metodundan farkli olarak bu metod motorlarin hiz ve konum kontrolunu
+saglayarak dogrusal olarak hareket etmesi saglanmistir.
 */
-void EtkinClass::movePid(uint8_t direction, int speed, int distance)
+void EtkinClass::movePid(uint8_t _direction, int _speed, int distance)
 {
 
-	motor_speed_theoric = map(speed, 0, 255, 0, 1023);
-	//SetpointMotor1 = motor_speed_theoric;
-	//SetpointMotor2 = motor_speed_theoric;
-	EtkinClass::move(direction, speed);
+	motor_speed_theoric = map(_speed, 0, 255, 0, 1023);
+	SetpointMotor1 = motor_speed_theoric;
+	SetpointMotor2 = motor_speed_theoric;
+	EtkinClass::move(_direction, _speed);
 	digitalWrite(rgb_red, LOW);
 	digitalWrite(rgb_green, LOW);
 	digitalWrite(rgb_blue, LOW);
+  isFinished = 0;
+	distance1 = 0;
+	distance2 = 0;
+	distancein = distance;
+  distancein = distancein / 2.953;
+	isFinished = 0;
+	timer0	= micros();
+	timer3	= micros();
+	uint8_t _direction_1, _direction_2;
+
+  if(_direction == 1)
+  {
+    _direction_1 = 1;
+    _direction_2 = 1;
+  }
+  else if(_direction == 2)
+  {
+    _direction_1 = 2;
+    _direction_2 = 2;
+  }
+  else if(_direction == 3)
+  {
+    _direction_1 = 2;
+    _direction_2 = 1;
+  }
+  else if(_direction == 4)
+  {
+    _direction_1 = 1;
+    _direction_2 = 2;
+  }
 
 	while (1)
   {
-    InputMotor1 = map(motor_speed, 0, 420, 0, 1023);
-    InputMotor2 = map(motor_speed1, 0, 420, 0, 1023);
+    InputMotor1 = map(motor1_speed, 0, 420, 0, 1023);
+    InputMotor2 = map(motor2_speed, 0, 420, 0, 1023);
     myPID1->Compute();
     myPID2->Compute();
-
-/*
-    pwmWriteDistance(Output1, direction, (distance / 2.953));
+	if (one_loop == 0)
+	{
+		analogWrite(motor1_pwm, 150);
+		analogWrite(motor2_pwm, 150);
+		delay(50);
+		one_loop = 1;
+	}
+    pwmWriteDistance_1(OutputMotor1, _direction_1, distancein );
+    pwmWriteDistance_2(OutputMotor2, _direction_2, distancein );
     speed_control();
-
-		if(Output1 < 80 && Output < 80)
-		{
-			break;
-		}
-*/
+    if(isFinished == 1)
+    {
+      break;
+    }
   }
+	digitalWrite(motor1_pwm, LOW);
+	digitalWrite(motor2_pwm, LOW);
 }
 
 /*joystick()**********************************************************/
@@ -338,11 +393,14 @@ ve pin3'e baglanmistir.
 void initINT()
 {
 	//Pin 2 ve 3 giris olarak ayarlandi
-	DDRD &= ~(1 << 2);
-	DDRD &= ~(1 << 3);
+	// DDRD &= ~(1 << 2);
+	// DDRD &= ~(1 << 3);
 	//Pin 2 ve 3 pullup
-	PORTD |= (1 << 2);
-	PORTD |= (1 << 3);
+	// PORTD |= (1 << 2);
+	// PORTD |= (1 << 3);
+
+	pinMode(2, INPUT);
+	pinMode(3, INPUT);
 	//Int0 ve Int1 icin dusen kenar tetiklemesi
 	EICRA |= (1 << ISC01);
 	EICRA |= (1 << ISC11);
@@ -373,7 +431,7 @@ void EtkinClass::pwmWriteDistance_1(int out, int dir, int dist)
   }
   else
   {
-    stop(0);
+    stopp(0);
   }
 }
 
@@ -394,25 +452,48 @@ void EtkinClass::pwmWriteDistance_2(int out, int dir, int dist)
   }
   else
   {
-    stop(0);
+    stopp(0);
   }
 }
-
-void EtkinClass::stop(bool urgentStop)
+/*stopp()**********************************************************************
+* urgentStop degiskeni ile motorlarlar eger speed_control() metodu tarafindan
+durduruldu ise Kirmizi led yanacaktir.
+*eger pwmWriteDistance metodlarindan durduruldu ise Yesil led yanacaktır.
+*/
+void EtkinClass::stopp(bool urgentStop)
 {
-	digitalWrite(motor1_pwm, LOW);
-	digitalWrite(motor2_pwm, LOW);
+		digitalWrite(motor1_pwm, LOW);
+		digitalWrite(motor2_pwm, LOW);
 
 	if(urgentStop)
 	{
 		digitalWrite(rgb_red, HIGH);
 		digitalWrite(rgb_green, LOW);
 		digitalWrite(rgb_blue, LOW);
+		digitalWrite(motor1_pwm, LOW);
+		digitalWrite(motor2_pwm, LOW);
+		delay(200);
 	}
 	else
 	{
 		digitalWrite(rgb_red, LOW);
 		digitalWrite(rgb_green, HIGH);
 		digitalWrite(rgb_blue, LOW);
+		isFinished = 1;
+		digitalWrite(motor1_pwm, LOW);
+		digitalWrite(motor2_pwm, LOW);
+		delay(200);
 	}
+}
+
+/*speed_control()***************************************************************
+*motorlardan herhangi biri dışarıdan müdehale ile durdugunda her iki motoruda
+durdurur.
+*/
+void EtkinClass::speed_control()
+{
+  if (((micros() - timer0) > 80000) || ((micros() - timer3) > 80000))
+  {
+    stopp(1);
+  }
 }
